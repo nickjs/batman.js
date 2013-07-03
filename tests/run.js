799157449c11ed998ff4299d86f3927fd27b4887
Justin Li <jli.justinli@gmail.com>
2013-07-03 18:03:50 -0400

    Compile and minify

--- tests/run.js
+++ tests/run.js
@@ -1,12 +1,17 @@
 (function() {
-  var coffee, glob, path, qqunit;
-  var __hasProp = Object.prototype.hasOwnProperty;
+  var coffee, glob, path, qqunit,
+    __hasProp = {}.hasOwnProperty;
+
   glob = require('glob');
+
   path = require('path');
+
   coffee = require('coffee-script-mapped');
+
   qqunit = require('qqunit');
+
   qqunit.Environment.jsdom.jQueryify(window, path.join(__dirname, 'lib', 'jquery.js'), function(window, jQuery) {
-    var Helper, k, tests, v;
+    var Helper, e, k, tests, v;
     global.jQuery = jQuery;
     try {
       require('./lib/query_selector_polyfill');
@@ -28,7 +33,8 @@
       tests = glob.sync("" + __dirname + "/batman/**/*_test.coffee").map(function(test) {
         return path.resolve(process.cwd(), test);
       });
-    } catch (e) {
+    } catch (_error) {
+      e = _error;
       console.error(e.stack);
       process.exit(1);
     }
@@ -37,4 +43,5 @@
       return process.exit(stats.failed);
     });
   });
+
 }).call(this);
